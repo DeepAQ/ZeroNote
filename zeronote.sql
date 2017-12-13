399ee4737a3fbece871eb4f879cc56bc715573d5
DeepAQ <i@aq741.com>
2017-12-13 14:46:35 +0800

    final: remove unused files, add README and LICENSE

--- zeronote.sql
+++ zeronote.sql
@@ -1,5 +1,3 @@
-BEGIN TRANSACTION;
-
 DROP TABLE IF EXISTS "user";
 CREATE TABLE 'user' (
     'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -7,8 +5,7 @@ CREATE TABLE 'user' (
     'password' TEXT NOT NULL,
     'nickname' TEXT,
     'created' INTEGER NOT NULL DEFAULT (strftime('%s','now'))
-);
-
+, 'disabled' BOOLEAN NOT NULL DEFAULT 0);
 DROP TABLE IF EXISTS "notebook";
 CREATE TABLE 'notebook' (
     'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -16,10 +13,17 @@ CREATE TABLE 'notebook' (
     'name' TEXT NOT NULL,
     'created' INTEGER DEFAULT (strftime('%s','now'))
 );
-
-DROP INDEX IF EXISTS "idx_notebook_userid";
-CREATE INDEX 'idx_notebook_userid' ON "notebook" ("userid");
-
+DROP TABLE IF EXISTS "sharing";
+CREATE TABLE 'sharing' (
+    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    'noteid' INTEGER NOT NULL,
+    'touserid' INTEGER NOT NULL,
+    'permission' INTEGER NOT NULL DEFAULT 0
+);
+DROP TABLE IF EXISTS "upvote";
+CREATE TABLE 'upvote' ('id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 'userid' INTEGER NOT NULL, 'noteid' INTEGER NOT NULL);
+DROP TABLE IF EXISTS "follow";
+CREATE TABLE 'follow' ('id' INTEGER PRIMARY KEY NOT NULL, 'fromuserid' INTEGER NOT NULL, 'touserid' INTEGER NOT NULL);
 DROP TABLE IF EXISTS "note";
 CREATE TABLE 'note' (
     'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -28,25 +32,22 @@ CREATE TABLE 'note' (
     'title' TEXT,
     'content' TEXT,
     'public' BOOLEAN NOT NULL DEFAULT 0,
-    'upvote' INTEGER NOT NULL DEFAULT 0,
     'created'  INTEGER NOT NULL DEFAULT (strftime('%s','now')),
     'updated'  INTEGER NOT NULL DEFAULT (strftime('%s','now'))
-);
-
-DROP INDEX IF EXISTS "idx_note_nbid";
-CREATE INDEX 'idx_note_nbid' ON "note" ("nbid");
-
-DROP TABLE IF EXISTS "sharing";
-CREATE TABLE 'sharing' (
-    'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    'noteid' INTEGER NOT NULL,
-    'touserid' INTEGER NOT NULL,
-    'permission' INTEGER NOT NULL DEFAULT 0
-);
-
+, 'tags' TEXT);
+DROP INDEX IF EXISTS "idx_notebook_userid";
+CREATE INDEX 'idx_notebook_userid' ON "notebook" ("userid");
 DROP INDEX IF EXISTS "idx_sharing_noteid";
 CREATE INDEX 'idx_sharing_noteid' ON "sharing" ("noteid");
 DROP INDEX IF EXISTS "idx_sharing_touserid";
 CREATE INDEX 'idx_sharing_touserid' ON "sharing" ("touserid");
-
-COMMIT;
+DROP INDEX IF EXISTS "idx_upvote_noteid";
+CREATE INDEX 'idx_upvote_noteid' ON "upvote" ("noteid");
+DROP INDEX IF EXISTS "idx_follow_fromuserid";
+CREATE INDEX 'idx_follow_fromuserid' ON "follow" ("fromuserid");
+DROP INDEX IF EXISTS "idx_follow_touserid";
+CREATE INDEX 'idx_follow_touserid' ON "follow" ("touserid");
+DROP INDEX IF EXISTS "idx_note_nbid";
+CREATE INDEX 'idx_note_nbid' ON "note" ("nbid");
+DROP INDEX IF EXISTS "idx_note_userid";
+CREATE INDEX 'idx_note_userid' ON "note" ("userid");
